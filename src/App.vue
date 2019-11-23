e6720f9f330530f0a414bf077a3f57d40b4b5cd6
Galdin Raphael <gldraphael@gmail.com>
2019-11-23 23:35:00 +0530

    Move stuff around a bit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+     <section class="section">
+      <div class="container">
+        <h1 class="title is-1">The Unescaper</h1>
+        <h2 class="subtitle">
+          A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading
+        </h2>
+      </div>
+    </section>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
 
 @Component({
   components: {
-    HelloWorld
+
   }
 })
-export default class App extends Vue {}
-</script>
+export default class App extends Vue {
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
-</style>
+</script>
