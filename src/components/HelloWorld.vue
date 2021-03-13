5a3d311ab738cbd4a80ad8e48f6cab2362b4ceb7
Galdin Raphael <gldraphael@users.noreply.github.com>
2021-03-13 15:32:55 +0530

    Upgrade to vue3

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -111,9 +111,9 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator"
+import { Prop } from "vue-property-decorator"
+import { Vue } from "vue-class-component"
 
-@Component
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string
 }
