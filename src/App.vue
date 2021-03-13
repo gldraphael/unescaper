8a70adc0a75ebfc33783f914c1a9b89b1d6d6a50
Galdin Raphael <gldraphael@users.noreply.github.com>
2021-03-13 17:13:44 +0530

    Adjust the sizes of things

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,22 @@
 <template>
-  <div id="app" class="max-w-xl mx-auto p-4">
-    <section>
-      <h1 class="text-2xl font-bold">The Unescaper</h1>
+  <div id="app" class="max-w-screen-lg mx-auto p-12">
+      <h1 class="text-4xl font-bold">The Unescaper</h1>
       <textarea
-        class="w-full mt-4 font-mono text-xs rounded"
-        rows="10"
+        class="w-full mt-8 font-mono text-sm rounded"
+        rows="16"
         placeholder="Paste text to be unescaped here."
         v-model="inputText"
         @input="inputChanged()"
       ></textarea>
 
-      <label class="block mt-2 mb-1 text-xs uppercase font-bold text-gray-400">Unescaped text</label>
+      <label class="block mt-4 mb-2 text-sm uppercase font-bold text-gray-400">Unescaped text</label>
       <textarea
-        class="w-full font-mono text-xs rounded"
-        rows="10"
+        class="w-full font-mono text-sm rounded"
+        rows="16"
         placeholder="The unescaped text will appear here."
         readonly
         v-model="escapedText"
       ></textarea>
-    </section>
   </div>
 </template>
 
