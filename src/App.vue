4c94a58ecbddd8ad98b12dd6e6d8802dbe475bc4
Galdin Raphael <gldraphael@users.noreply.github.com>
2021-03-13 17:00:59 +0530

    Style the app reasonably

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,23 @@
 <template>
-  <div id="app">
-    <section class="section">
-      <div class="container">
-        <h1 class="title is-1">The Unescaper</h1>
-      </div>
-      <div class="container">
-        <div class="field">
-          <label class="label">Enter text to be unescaped</label>
-          <div class="control">
-            <textarea
-              class="textarea is-primary"
-              rows="10"
-              placeholder="Paste text to be unescaped here."
-              v-model="inputText"
-              @input="inputChanged()"
-            ></textarea>
-          </div>
-        </div>
+  <div id="app" class="max-w-xl mx-auto p-4">
+    <section>
+      <h1 class="text-2xl font-bold">The Unescaper</h1>
+      <textarea
+        class="w-full mt-4 font-mono text-xs rounded"
+        rows="10"
+        placeholder="Paste text to be unescaped here."
+        v-model="inputText"
+        @input="inputChanged()"
+      ></textarea>
 
-        <div class="field">
-          <label class="label">Unescaped text</label>
-          <div class="control">
-            <textarea
-              class="textarea is-primary"
-              rows="10"
-              placeholder="The unescaped text will appear here."
-              readonly
-              v-model="escapedText"
-            ></textarea>
-          </div>
-        </div>
-      </div>
+      <label class="block mt-2 mb-1 text-xs uppercase font-bold text-gray-400">Unescaped text</label>
+      <textarea
+        class="w-full font-mono text-xs rounded"
+        rows="10"
+        placeholder="The unescaped text will appear here."
+        readonly
+        v-model="escapedText"
+      ></textarea>
     </section>
   </div>
 </template>
