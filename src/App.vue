638b18a09d4f4225327d837d44cc34923648686c
Galdin Raphael <gldraphael@users.noreply.github.com>
2021-03-13 17:14:17 +0530

    Fix all the lint errors

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <template>
   <div id="app" class="max-w-screen-lg mx-auto p-12">
-      <h1 class="text-4xl font-bold">The Unescaper</h1>
-      <textarea
-        class="w-full mt-8 font-mono text-sm rounded"
-        rows="16"
-        placeholder="Paste text to be unescaped here."
-        v-model="inputText"
-        @input="inputChanged()"
-      ></textarea>
+    <h1 class="text-4xl font-bold">The Unescaper</h1>
+    <textarea
+      class="w-full mt-8 font-mono text-sm rounded"
+      rows="16"
+      placeholder="Paste text to be unescaped here."
+      v-model="inputText"
+      @input="inputChanged()"
+    ></textarea>
 
-      <label class="block mt-4 mb-2 text-sm uppercase font-bold text-gray-400">Unescaped text</label>
-      <textarea
-        class="w-full font-mono text-sm rounded"
-        rows="16"
-        placeholder="The unescaped text will appear here."
-        readonly
-        v-model="escapedText"
-      ></textarea>
+    <label class="block mt-4 mb-2 text-sm uppercase font-bold text-gray-400"
+      >Unescaped text</label
+    >
+    <textarea
+      class="w-full font-mono text-sm rounded"
+      rows="16"
+      placeholder="The unescaped text will appear here."
+      readonly
+      v-model="escapedText"
+    ></textarea>
   </div>
 </template>
 
